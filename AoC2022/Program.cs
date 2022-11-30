15810b4588b57a8e28cd292551b6839bb5b5b262
Srb František <srb.frank@gmail.com>
2022-12-01 00:58:12 +0100

    2022 prep

---
+++ AoC2022/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AoC2022
+{
+
+    class Program
+    {
+        static PuzzleType activePuzzle = ;
+        enum PuzzleType
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            switch (activePuzzle)
+            {
+                default:
+                    break;
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.ElapsedMilliseconds}ms");
+        }
+    }
+}
---
+++ AoC2022/Puzzle1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC2022
+{
+    class Puzzle1
+    {
+        static public void SolvePuzzle()
+        {
+
+        }
+    }
+}
