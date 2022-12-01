d43fd8469bc0c9a181b041ba0d297c475ba6855d
Srb František <srb.frank@gmail.com>
2022-12-01 10:55:13 +0100

    day 1

--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -5,16 +5,19 @@ namespace AoC2022
 
     class Program
     {
-        static PuzzleType activePuzzle = ;
+        static PuzzleType activePuzzle = PuzzleType.Calories;
         enum PuzzleType
         {
-
+            Calories
         }
         static void Main(string[] args)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
             switch (activePuzzle)
             {
+                case PuzzleType.Calories:
+                    Puzzle1.SolvePuzzle();
+                    break;
                 default:
                     break;
             }
--- AoC2022/Puzzle1.cs
+++ AoC2022/Puzzle1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace AoC2022
 {
@@ -8,7 +9,31 @@ namespace AoC2022
     {
         static public void SolvePuzzle()
         {
-
+            string puzzePath = "inputs/input1.txt";
+            StreamReader sr = new StreamReader(puzzePath);
+            LinkedList<uint> max_elfs = new LinkedList<uint>();
+            for (int i = 0; i < 3; i++)
+                max_elfs.AddLast(0);
+            string input;
+            while (!sr.EndOfStream)
+            {
+                uint sum = 0;
+                while ((input = sr.ReadLine()) != "" && input != null)
+                    sum += uint.Parse(input);
+                LinkedListNode<uint> current = max_elfs.First;
+                while(current != null)
+                {
+                    if (sum > current.Value)
+                    {
+                        max_elfs.AddBefore(current, sum);
+                        max_elfs.RemoveLast();
+                        break;
+                    }
+                    current = current.Next;
+                }
+            }
+            Console.WriteLine($"1st elf   : {max_elfs.First.Value}");
+            Console.WriteLine($"top 3 elfs: {max_elfs.Sum(n => n)}");
         }
     }
 }
