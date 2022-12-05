168dbf8bfe6a2382de4f64125f7b3f0568ede678
Srb František <srb.frank@gmail.com>
2022-12-05 10:21:34 +0100

    day 5 fix

--- AoC2022/Puzzle05.py
+++ AoC2022/Puzzle05.py
@@ -10,8 +10,7 @@ for l in open('inputs/input05.txt'):
             inp.append(l[1::4])
         else:
             towersCount = int(l.split()[-1])
-    else:
-        if l!="":
+    elif l!="":
             commands.append([int(x) for x in l.split()[1::2]])
 towers = []
 for _ in range(towersCount):
@@ -21,16 +20,15 @@ for t in inp:
     for i,c in enumerate(t):
         if c != ' ':
             towers[i].append(c)
-
 towers2 = copy.deepcopy(towers)
-
+#part 1
 for howMany,fromT,toT in commands:
     for _ in range(howMany):
         towers[toT-1].append(towers[fromT-1].pop())
-
+#part 2
 for howMany,fromT,toT in commands:
     towers2[toT-1].extend(towers2[fromT-1][-howMany:])
-    for _ in range(howMany):
-        towers2[fromT-1].pop()
+    towers2[fromT-1] = towers2[fromT-1][:-howMany]
+
 print("".join([t.pop() for t in towers]))
 print("".join([t.pop() for t in towers2]))
