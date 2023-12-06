b9d3142de86f197994e035edbfa7b65486ded039
Frantisek Srb <srb.frank@gmail.com>
2023-12-06 09:41:17 +0100

    format d6

--- 23AoC/Puzzles/Puzzle6.cpp
+++ 23AoC/Puzzles/Puzzle6.cpp
@@ -1,7 +1,8 @@
 #include "../Utility.h"
 #include "../Puzzle.h"
 
-class Puzzle6 : IPuzzle {
+class Puzzle6 : IPuzzle
+{
 	void Solve() override
 	{
 		long ways = 1;
@@ -9,25 +10,25 @@ class Puzzle6 : IPuzzle {
 		{
 			long time = std::stol(_inputWords[0][i]), dist = std::stol(_inputWords[1][i]);
 			int w = 0;
-			while (w * (time-w) <= dist)
+			while (w * (time - w) <= dist)
 				w++;
-			ways *= time + 1 - 2*w;
+			ways *= time + 1 - 2 * w;
 		}
 		LOG("Result: " << ways)
-		
 	}
 	void SolveAdvanced() override
 	{
-		string t,d;
+		string t, d;
 		for (size_t i = 1; i < _inputWords[0].size(); i++)
 		{
 			t += _inputWords[0][i];
 			d += _inputWords[1][i];
 		}
 		size_t time = std::stoull(t), dist = std::stoull(d);
-		size_t w = time/2, step = w/2;
-		while (!(w * (time - w) > dist && (w-1) * (time - w+1) <= dist)){
-			w += w* (time - w) > dist ? -step : step;
+		size_t w = time / 2, step = w / 2;
+		while (!(w * (time - w) > dist && (w - 1) * (time - w + 1) <= dist))
+		{
+			w += w * (time - w) > dist ? -step : step;
 			step /= 2;
 		}
 		LOG("Result: " << time + 1 - 2 * w)
